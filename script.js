781554adf9fb7831654c7ce4e2fabd9b27cac05b
S7VIK <miw6515@gmail.com>
2026-06-13 17:35:57 +0530

    Update status messages for JavaScript and TypeScript

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-document.getElementById("js-test").innerText = "JS Status: ON ✅ | Learning in Progress 🚀";
+document.getElementById("js-test").innerText =
+  "🟨 JavaScript | Status: Active Learning 🚀 | Focus: DOM • ES6+ • Async JS • Projects | Goal: Build interactive web applications";
+
+document.getElementById("ts-test").innerText =
+  "🔷 TypeScript | Status: Getting Started 📚 | Focus: Types • Interfaces • Generics | Goal: Write scalable and maintainable code";
